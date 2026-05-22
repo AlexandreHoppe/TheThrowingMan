565a8c485557946de002b81f1478c05e316d8206
Alexandre Hoppe <alexandreahoppe@hotmail.com>
2026-05-22 16:48:16 +0200

    Add the formula for the trajectory of the stone.

--- ThrowingMan.py
+++ ThrowingMan.py
@@ -1,16 +1,31 @@
-rows, cols = (10, 20)
+import time
+
+rows, cols = (50, 100)
 visualList = [[" " for i in range(cols)] for j in range(rows)]
 visualList[-1] = ["_"]*cols * 1
 
-visualList[-3][1] = "/"
-visualList[-1][1]= "/"
+visualList[-3][0] = "/"
+visualList[-1][0]= "/"
 
-visualList[-3][2] = "\\"
-visualList[-1][2]= "\\"
+visualList[-3][1] = "\\"
+visualList[-1][1]= "\\"
 
-visualList[-2][1]= " |"
-
-visualList[-4][1]= " O"
+visualList[-2][0]= " |"
+visualList[-2][1]= "*"
+visualList[-4][0]= " 0"
 
 for w in range(len(visualList)) :
     print(*visualList[w], end="\n")
+
+
+throwForce = int(input("Enter the Throwing strenght : "))
+
+for x in range(throwForce * 2) :
+    if x > throwForce :
+        
+    yPos = x**2 + 2
+    visualList[-x - 2][yPos-1]= "*" 
+    time.sleep(1)
+
+    for w in range(len(visualList)) :
+        print(*visualList[w], end="\n")
